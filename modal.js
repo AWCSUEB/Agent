23810e28a3a729d51696c9dd848bebe0019581a8
ajwoodall <ajwoodall@gmail.com>
2016-10-29 00:42:33 -0700

    .

---
+++ modal.js
@@ -0,0 +1,9 @@
+/* http://blog.lavgupta.com/2011/12/jquery-modal-waiting-dialog.html */
+
+function ShowDialog() {
+    $("#loading-div").show();
+}
+
+function HideDialog() {
+    $("#loading-div").hide();
+}
